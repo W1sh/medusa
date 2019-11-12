fb32058c9daa3e9f28f56d29c5f4684e1ca29080
Bruno Miguel Ferreira <bruno.miguel.ferreira@everis.com>
2019-11-12 16:42:25 +0000

    implementação de filtração por role no MessageCreateListener

--- src/main/java/com/w1sh/medusa/listeners/MessageCreateListener.java
+++ src/main/java/com/w1sh/medusa/listeners/MessageCreateListener.java
@@ -1,13 +1,13 @@
 package com.w1sh.medusa.listeners;
 
 import discord4j.core.event.domain.message.MessageCreateEvent;
-import discord4j.core.object.entity.Guild;
-import discord4j.core.object.entity.TextChannel;
+import discord4j.core.object.entity.Role;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.util.function.Tuple2;
+
+import java.util.Objects;
 
 @Slf4j
 @Component
@@ -20,13 +20,16 @@ public class MessageCreateListener implements EventListener<MessageCreateEvent> 
 
     @Override
     public Mono<Void> execute(MessageCreateEvent event) {
-        return event.getGuild()
-                .flatMapMany(Guild::getChannels)
-                .ofType(TextChannel.class)
-                .filter(textChannel -> textChannel.getName().toLowerCase().contains("general"))
-                .collectList()
-                .doOnNext(textChannels -> Flux.fromIterable(textChannels)
-                        .flatMap(textChannel -> textChannel.createMessage("Welcome"))
+        return event.getMessage()
+                .getChannel()
+                .zipWith(event.getMessage().getAuthorAsMember())
+                .filterWhen(tuple -> tuple.getT2().getRoles()
+                            .filter(Objects::nonNull)
+                            .map(Role::getName)
+                            .doOnEach(role -> log.info("Role {}", role.get()))
+                            .any(role -> role.toLowerCase().contains("admin")))
+                .map(Tuple2::getT1)
+                .doOnNext(textChannel -> textChannel.createMessage("Welcome")
                         .elapsed()
                         .map(Tuple2::getT1)
                         .doOnNext(elapsed -> log.info("Answered request in {} milliseconds", elapsed))
