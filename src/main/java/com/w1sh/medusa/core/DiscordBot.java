bd4d047497035d0076bc170b4427a26cb72feaf1
W1sh <brunoferreira.ebm@gmail.com>
2019-11-18 20:48:23 +0000

    groundwork for publish on command event dispatcher

--- src/main/java/com/w1sh/medusa/core/DiscordBot.java
+++ src/main/java/com/w1sh/medusa/core/DiscordBot.java
@@ -1,9 +1,12 @@
 package com.w1sh.medusa.core;
 
+import com.w1sh.medusa.core.dispatchers.CommandEventDispatcher;
 import com.w1sh.medusa.listeners.EventListener;
 import com.w1sh.medusa.listeners.impl.*;
 import discord4j.core.DiscordClient;
 import discord4j.core.event.domain.Event;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -21,16 +24,18 @@ public class DiscordBot {
     private final VoiceStateUpdateListener voiceStateUpdateListener;
     private final ReadyListener readyListener;
     private final DisconnectListener disconnectListener;
+    private final CommandEventDispatcher commandEventDispatcher;
 
     public DiscordBot(DiscordClient client, GenericEventListener genericEventListener,
                       MessageCreateListener messageCreateListener, VoiceStateUpdateListener voiceStateUpdateListener,
-                      ReadyListener readyListener, DisconnectListener disconnectListener) {
+                      ReadyListener readyListener, DisconnectListener disconnectListener, CommandEventDispatcher commandEventDispatcher) {
         this.client = client;
         this.genericEventListener = genericEventListener;
         this.messageCreateListener = messageCreateListener;
         this.voiceStateUpdateListener = voiceStateUpdateListener;
         this.readyListener = readyListener;
         this.disconnectListener = disconnectListener;
+        this.commandEventDispatcher = commandEventDispatcher;
     }
 
     @PostConstruct
@@ -40,6 +45,8 @@ public class DiscordBot {
         setupEventDispatcher(disconnectListener);
         setupEventDispatcher(readyListener);
         setupEventDispatcher(voiceStateUpdateListener);
+
+        setupCommandEventDispatcher();
 
         client.login().block();
     }
@@ -52,5 +59,14 @@ public class DiscordBot {
                 .ofType(eventListener.getEventType())
                 .flatMap(event -> eventListener.execute(client, event))
                 .subscribe(null, throwable -> logger.error("Error when consuming events", throwable));
+    }
+
+    private void setupCommandEventDispatcher(){
+        client.getEventDispatcher()
+                .on(MessageCreateEvent.class)
+                .filter(event -> event.getMember().isPresent())
+                .filter(event -> event.getMember().map(User::isBot).orElse(false))
+                .filter(event -> event.getMessage().getContent().orElse("").startsWith("!"))
+                .doOnNext(commandEventDispatcher::publish);
     }
 }
