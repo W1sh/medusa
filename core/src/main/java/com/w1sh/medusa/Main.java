71f19cbcabd227a9b84e7125000411c8bb2321ac
W1sh <brunoferreira.ebm@gmail.com>
2019-12-21 22:02:36 +0000

    Remove unused AOP dependency

--- core/src/main/java/com/w1sh/medusa/Main.java
+++ core/src/main/java/com/w1sh/medusa/Main.java
@@ -5,12 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 @PropertySource(value = "text-constants.properties")
-@EnableAspectJAutoProxy
 public class Main {
 
     public static void main(String[] args) {
