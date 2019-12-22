b9a45e82bc4f297e3cfae30f2602c7d082d25dd2
W1sh <brunoferreira.ebm@gmail.com>
2019-12-22 21:35:46 +0000

    Add pre-validation to change prefix event

--- core/src/main/java/com/w1sh/medusa/api/misc/listeners/ChangePrefixEventListener.java
+++ core/src/main/java/com/w1sh/medusa/api/misc/listeners/ChangePrefixEventListener.java
@@ -5,6 +5,8 @@ import com.w1sh.medusa.core.dispatchers.CommandEventDispatcher;
 import com.w1sh.medusa.core.events.EventFactory;
 import com.w1sh.medusa.core.listeners.MultipleArgsEventListener;
 import com.w1sh.medusa.utils.Messenger;
+import discord4j.core.object.presence.Activity;
+import discord4j.core.object.presence.Presence;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
@@ -28,13 +30,17 @@ public class ChangePrefixEventListener implements MultipleArgsEventListener<Chan
                 .map(ev -> ev.getMessage().getContent().orElse("").split(" ")[1])
                 .doOnNext(prefix -> {
                     EventFactory.setPrefix(prefix);
-                    Messenger.send(event, String.format("Change prefix to \"%s\"", prefix)).subscribe();
+                    Messenger.send(event, String.format("Changed prefix to \"%s\"", prefix)).subscribe();
+                    event.getClient().updatePresence(Presence.online(Activity.watching(String.format("Cringe 2 | %shelp", EventFactory.getPrefix()))));
                 })
                 .then();
     }
 
     @Override
     public Mono<Boolean> validate(ChangePrefixEvent event) {
-        return Mono.justOrEmpty(true);
+        return Mono.justOrEmpty(event.getMessage().getContent())
+                .map(message -> message.split(" "))
+                .filter(strings -> strings.length == 2)
+                .hasElement();
     }
 }
