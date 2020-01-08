f2dd01c8f060671ca4d2f3a5197b0cebaf62594a
W1sh <brunoferreira.ebm@gmail.com>
2020-01-08 21:08:16 +0000

    Allow caching of retrieved cards from Scryfall

--- mtg/src/main/java/com/w1sh/medusa/services/impl/CardServiceImpl.java
+++ mtg/src/main/java/com/w1sh/medusa/services/impl/CardServiceImpl.java
@@ -1,17 +1,25 @@
 package com.w1sh.medusa.services.impl;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import com.w1sh.medusa.resources.Card;
 import com.w1sh.medusa.resources.ListResponse;
 import com.w1sh.medusa.rest.CardClient;
 import com.w1sh.medusa.services.CardService;
 import org.springframework.stereotype.Component;
+import reactor.cache.CacheMono;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.Signal;
+import reactor.core.scheduler.Schedulers;
+
+import java.util.Optional;
 
 @Component
 public class CardServiceImpl implements CardService {
 
     private final CardClient cardClient;
+    private final Cache<String, Card> cardCache = Caffeine.newBuilder().build();
 
     public CardServiceImpl(CardClient cardClient) {
         this.cardClient = cardClient;
@@ -19,7 +27,16 @@ public class CardServiceImpl implements CardService {
 
     @Override
     public Mono<Card> getCardByName(String name) {
-        return cardClient.getCardByName(name);
+        return CacheMono.lookup(key -> Mono.justOrEmpty(cardCache.getIfPresent(key))
+                .map(Signal::next), name)
+                .onCacheMissResume(
+                        () -> cardClient.getCardByName(name)
+                                .subscribeOn(Schedulers.elastic()))
+                .andWriteWith(
+                        (key, signal) -> Mono.fromRunnable(
+                                () -> Optional.ofNullable(signal.get())
+                                        .ifPresent(value -> cardCache.put(key, value))));
+        /*return cardClient.getCardByName(name);*/
     }
 
     @Override
