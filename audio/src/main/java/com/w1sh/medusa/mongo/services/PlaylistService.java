0596c66c8460d24e1ec14e4f1df7c54ad5d6d525
W1sh <brunoferreira.ebm@gmail.com>
2020-01-19 21:43:48 +0000

    Add cache for playlists

--- audio/src/main/java/com/w1sh/medusa/mongo/services/PlaylistService.java
+++ audio/src/main/java/com/w1sh/medusa/mongo/services/PlaylistService.java
@@ -1,18 +1,28 @@
 package com.w1sh.medusa.mongo.services;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import com.w1sh.medusa.mongo.entities.Playlist;
 import com.w1sh.medusa.mongo.repos.PlaylistRepo;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
+import java.util.List;
+
 @Service
 public final class PlaylistService {
 
     private final PlaylistRepo playlistRepo;
+    private final Cache<String, List<Playlist>> playlistsCache;
 
     public PlaylistService(PlaylistRepo playlistRepo) {
         this.playlistRepo = playlistRepo;
+        this.playlistsCache = Caffeine.newBuilder()
+                .expireAfterAccess(Duration.ofHours(6))
+                .recordStats()
+                .build();
     }
 
     public Flux<Playlist> findAll(){
