c75975cfe808e2f0ece6dc98458df19c2e15067f
W1sh <brunoferreira.ebm@gmail.com>
2020-01-25 14:17:53 +0000

    Refactor duel event and disable duel roll for future refactor

--- core/src/main/java/com/w1sh/medusa/api/dice/Dice.java
+++ core/src/main/java/com/w1sh/medusa/api/dice/Dice.java
@@ -1,8 +1,12 @@
 package com.w1sh.medusa.api.dice;
 
+import com.w1sh.medusa.core.data.TextMessage;
+import com.w1sh.medusa.core.dispatchers.ResponseDispatcher;
+import com.w1sh.medusa.core.events.Event;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
 
 import java.util.Random;
 
@@ -10,18 +14,44 @@ import java.util.Random;
 public class Dice {
 
     private static final Logger logger = LoggerFactory.getLogger(Dice.class);
-    private final Random random;
+    public static final String ROLL_ARGUMENT_DELIMITER = "-";
 
-    public Dice(Random random){
+    private final Random random;
+    private final ResponseDispatcher responseDispatcher;
+
+    public Dice(Random random, ResponseDispatcher responseDispatcher){
         this.random = random;
+        this.responseDispatcher = responseDispatcher;
     }
 
-    public Integer roll(String min, String max) {
-        try {
-            return random.nextInt((Integer.parseInt(min) + Integer.parseInt(max) + 1)) + Integer.parseInt(min);
-        } catch (NumberFormatException e){
-            logger.info("Failed to parse string to number", e);
-            return 0;
-        }
+    public Mono<Integer> roll(Integer min, Integer max){
+        return Mono.just(random.nextInt(min + max + 1) + min);
+    }
+
+    public Mono<Boolean> validateRollArgument(Event event){
+        return Mono.justOrEmpty(event)
+                .map(limits -> limits.getArguments().get(0).split(ROLL_ARGUMENT_DELIMITER))
+                .filter(limits -> limits.length == 2)
+                .map(limits -> new int[]{Integer.parseInt(limits[0]), Integer.parseInt(limits[1])})
+                .onErrorResume(throwable -> Mono.fromRunnable(
+                        () -> logger.error("Failed to parse arguments", throwable)))
+                .filter(limits -> limits[1] > limits[0])
+                .hasElement()
+                .flatMap(bool -> {
+                    if(Boolean.FALSE.equals(bool)){
+                        return createRollErrorMessage(event);
+                    }else return Mono.empty();
+                })
+                .hasElement()
+                .map(b -> !b);
+    }
+
+    private Mono<TextMessage> createRollErrorMessage(Event event){
+        return event.getMessage().getChannel()
+                .map(channel -> new TextMessage(channel,
+                        ":x: Invalid argument received, the argument must be of type **X-Y** where **Y** must be greater than **X**",
+                        false))
+                .doOnNext(responseDispatcher::queue)
+                .doAfterTerminate(responseDispatcher::flush);
     }
 }
