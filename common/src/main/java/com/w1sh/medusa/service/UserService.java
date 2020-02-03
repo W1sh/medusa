63d053c30fe785923d5d468e7738958c4d7f43b1
W1sh <brunoferreira.ebm@gmail.com>
2020-02-03 21:35:37 +0000

    Add users cache

--- common/src/main/java/com/w1sh/medusa/service/UserService.java
+++ common/src/main/java/com/w1sh/medusa/service/UserService.java
@@ -9,9 +9,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import reactor.cache.CacheMono;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.Signal;
+import reactor.core.scheduler.Schedulers;
 
 import java.time.Duration;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -34,12 +38,23 @@ public class UserService {
     }
 
     public Mono<User> save(User user){
-        return userRepository.save(user);
+        return userRepository.save(user)
+                .doOnNext(u -> usersCache.put(u.getUserId(), u));
     }
 
     public Mono<User> findByUserId(Long userId) {
-        return userRepository.findByUserId(String.valueOf(userId))
-                .defaultIfEmpty(new User(userId));
+        return CacheMono.lookup(key -> Mono.justOrEmpty(usersCache.getIfPresent(key))
+                .map(Signal::next), userId)
+                .onCacheMissResume(() -> userRepository.findByUserId(String.valueOf(userId))
+                        .defaultIfEmpty(new User(userId))
+                        .subscribeOn(Schedulers.elastic()))
+                .andWriteWith((key, signal) -> Mono.fromRunnable(
+                        () -> Optional.ofNullable(signal.get())
+                                .ifPresent(value -> usersCache.put(key, value))))
+                .onErrorResume(throwable -> {
+                    logger.error("Failed to user with id \"{}\"", userId, throwable);
+                    return Mono.just(new User(userId));
+                });
     }
 
     public Mono<Void> distributePoints(Member member) {
