1dc74f8eb633ef25dc333048caed25ec9a8bb097
W1sh <brunoferreira.ebm@gmail.com>
2020-07-23 18:43:57 +0100

    #105 - Migrate Warning to mongodb

--- moderation/src/main/java/com/w1sh/medusa/data/Warning.java
+++ moderation/src/main/java/com/w1sh/medusa/data/Warning.java
@@ -2,31 +2,37 @@ package com.w1sh.medusa.data;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.relational.core.mapping.Column;
-import org.springframework.data.relational.core.mapping.Table;
 
 import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
-@Table(value = "core.warnings")
+@Document
 public class Warning {
 
     @Id
-    private Integer id;
+    private String id;
 
-    @Column(value = "fk_user")
-    private User user;
+    @Column(value = "user_id")
+    private String userId;
 
     @Column(value = "channel_id")
     private String channelId;
 
+    @Column(value = "guild_id")
+    private String guildId;
+
+    @CreatedDate
     @Column(value = "created_on")
     private LocalDateTime createdOn;
 
-    public Warning(User user, String channelId) {
-        this.user = user;
+    public Warning(String userId, String channelId, String guildId) {
+        this.userId = userId;
         this.channelId = channelId;
+        this.guildId = guildId;
     }
 }
--- moderation/src/main/java/com/w1sh/medusa/services/WarningService.java
+++ moderation/src/main/java/com/w1sh/medusa/services/WarningService.java
@@ -2,35 +2,29 @@ package com.w1sh.medusa.services;
 
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
+import com.mongodb.reactivestreams.client.MongoClients;
 import com.w1sh.medusa.data.Warning;
-import com.w1sh.medusa.repos.WarningRepository;
 import com.w1sh.medusa.utils.Caches;
 import com.w1sh.medusa.utils.Reactive;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import org.springframework.stereotype.Service;
-import reactor.cache.CacheMono;
 import reactor.core.publisher.Mono;
-import reactor.core.publisher.Signal;
 
 import java.time.Duration;
 import java.util.HashSet;
-import java.util.Optional;
 import java.util.Set;
-import java.util.function.Supplier;
-import java.util.stream.Collectors;
 
 @Service
 @Slf4j
 public class WarningService {
 
-    private final WarningRepository repository;
-    private final UserService userService;
-    private final Cache<Integer, Set<Warning>> warnings;
-    private final Cache<Integer, Warning> temporaryWarnings;
+    private final ReactiveMongoTemplate template;
+    private final Cache<String, Set<Warning>> warnings;
+    private final Cache<String, Warning> temporaryWarnings;
 
-    public WarningService(WarningRepository repository, UserService userService) {
-        this.repository = repository;
-        this.userService = userService;
+    public WarningService() {
+        this.template = new ReactiveMongoTemplate(MongoClients.create(), "test");
         this.warnings = Caffeine.newBuilder().build();
         this.temporaryWarnings =  Caffeine.newBuilder()
                 .expireAfterAccess(Duration.ofMinutes(30))
@@ -38,37 +32,20 @@ public class WarningService {
     }
 
     public Mono<Warning> save(Warning warning) {
-        return repository.save(warning)
-                .doOnNext(w -> Caches.storeMultivalue(w.getUser().getId(), w, warnings.asMap().getOrDefault(w.getUser().getId(), new HashSet<>()), warnings))
+        return template.save(warning)
+                .doOnNext(w -> Caches.storeMultivalue(w.getUserId(), w, warnings.asMap().getOrDefault(w.getUserId(), new HashSet<>()), warnings))
                 .flatMap(this::saveTemporary)
                 .onErrorResume(t -> Mono.fromRunnable(() -> log.error("Failed to save warning with id \"{}\"", warning.getId(), t)));
     }
 
     public Mono<Warning> saveTemporary(Warning warning) {
-        return Mono.just(warning).doOnNext(w -> temporaryWarnings.put(w.getUser().getId(), w))
+        return Mono.just(warning).doOnNext(w -> temporaryWarnings.put(w.getUserId(), w))
                 .onErrorResume(t -> Mono.fromRunnable(() -> log.error("Failed to save temporary warning with id \"{}\"", warning.getId(), t)));
     }
 
-    public Mono<Warning> addWarning(String userId, String channelId){
-        Mono<Warning> warning = Mono.defer(() -> userService.findByUserId(userId)
-                .map(user -> new Warning(user, channelId)))
-                .doOnNext(w -> log.info("Creating warning for user with id \"{}\" in channel \"{}\"", w.getUser().getUserId(), w.getChannelId()));
-
-        return userService.findByUserId(userId)
-                .filterWhen(user -> Mono.justOrEmpty(temporaryWarnings.getIfPresent(user.getId())).hasElement())
+    public Mono<Warning> addWarning(Warning warning){
+        return Mono.justOrEmpty(temporaryWarnings.getIfPresent(warning.getUserId()))
                 .hasElement()
-                .transform(Reactive.ifElse(bool -> warning.flatMap(this::save), bool -> warning.flatMap(this::saveTemporary)));
+                .transform(Reactive.ifElse(bool -> save(warning), bool -> saveTemporary(warning)));
     }
-
-    private Mono<Set<Warning>> findAllByUser(Integer userId){
-        final Supplier<Mono<Set<Warning>>> supplier = () -> repository.findAllByUser(userId)
-                .collect(Collectors.toSet())
-                .doOnNext(ws -> log.info("Fetched {} warnings from database for user with id {}", ws.size(), userId));
-
-        return CacheMono.lookup(key -> Mono.justOrEmpty(warnings.getIfPresent(key))
-                .map(Signal::next), userId)
-                .onCacheMissResume(supplier)
-                .andWriteWith((key, signal) -> Mono.fromRunnable(() -> Optional.ofNullable(signal.get()).ifPresent(value -> warnings.put(key, value))));
-    }
-
 }
