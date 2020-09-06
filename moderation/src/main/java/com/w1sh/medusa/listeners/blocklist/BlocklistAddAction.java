12ffe6287b909296747c1f200b362838b1d300be
W1sh <brunoferreira.ebm@gmail.com>
2020-09-06 18:04:59 +0100

    Introduce event and listener for blocklist
    Related #113

---
+++ moderation/src/main/java/com/w1sh/medusa/listeners/blocklist/BlocklistAddAction.java
@@ -0,0 +1,21 @@
+package com.w1sh.medusa.listeners.blocklist;
+
+import com.w1sh.medusa.data.responses.Response;
+import com.w1sh.medusa.events.BlocklistEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import java.util.function.Function;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public final class BlocklistAddAction implements Function<BlocklistEvent, Mono<? extends Response>> {
+
+    @Override
+    public Mono<? extends Response> apply(BlocklistEvent playlistEvent) {
+        return Mono.empty();
+    }
+}
