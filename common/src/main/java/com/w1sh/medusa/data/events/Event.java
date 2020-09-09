ae3cab9214da4d31536d05ba253b4fa650041f86
W1sh <brunoferreira.ebm@gmail.com>
2020-09-09 21:23:05 +0100

    Refactor Event to encapsulate the discord event
    This version offers most commonly used methods as getters.
    
    Warning! This is a commit that will not build. This is part of a bigger development to refactor the current implementation.
    
    Related #123

--- common/src/main/java/com/w1sh/medusa/data/events/Event.java
+++ common/src/main/java/com/w1sh/medusa/data/events/Event.java
@@ -1,61 +1,134 @@
 package com.w1sh.medusa.data.events;
 
 import discord4j.common.util.Snowflake;
+import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.message.MessageCreateEvent;
-import discord4j.rest.util.Permission;
+import discord4j.core.object.entity.Guild;
+import discord4j.core.object.entity.Member;
+import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.User;
+import discord4j.core.object.entity.channel.Channel;
+import discord4j.core.object.entity.channel.GuildChannel;
+import discord4j.core.object.entity.channel.MessageChannel;
+import discord4j.gateway.ShardInfo;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.checkerframework.checker.units.qual.A;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.mongodb.core.mapping.Document;
+import reactor.core.publisher.Mono;
 
+import java.time.Instant;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.Objects;
 
-public abstract class Event extends MessageCreateEvent {
+/**
+ * A representation of a command that was directed at the bot.
+ *
+ * {@link #guildId} and {@link #member} may not be present if the message was sent in a private channel.
+ */
+@Data
+@Document
+@NoArgsConstructor
+public class Event {
 
-    private Integer minArguments;
-    private Map<Integer, String> arguments;
-    private List<Permission> permissions;
+    @Id
+    private String id;
+
+    private String userId;
+
+    private String guildId;
+
+    @CreatedDate
+    private Instant createdOn;
+
+    @Transient
+    private Member member;
+
+    @Transient
+    private Message message;
+
+    @Transient
+    private ShardInfo shardInfo;
+
+    @Transient
+    private List<String> arguments;
+
+    @Transient
+    private GatewayDiscordClient client;
 
     public Event(MessageCreateEvent event){
-        super(event.getClient(), event.getShardInfo(), event.getMessage(),
-                event.getGuildId().map(Snowflake::asLong).orElseThrow(),
-                event.getMember().orElseThrow());
-        this.minArguments = 0;
-        this.arguments = new HashMap<>();
-        this.permissions = new ArrayList<>();
-        this.permissions.add(Permission.SEND_MESSAGES);
+        this.userId = event.getMember().map(User::getId).map(Snowflake::asString).orElse(null);
+        this.guildId = event.getGuildId().map(Snowflake::asString).orElse(null);
+        this.message = event.getMessage();
+        this.shardInfo = event.getShardInfo();
+        this.member = event.getMember().orElse(null);
+        this.arguments = new ArrayList<>();
+        this.client = event.getClient();
+
+        Objects.requireNonNull(userId);
+        Objects.requireNonNull(guildId);
     }
 
-    public Event(MessageCreateEvent event, Integer minArguments) {
+    public Event(MessageCreateEvent event, List<String> arguments) {
         this(event);
-        this.minArguments = minArguments;
-    }
-
-    public Event(MessageCreateEvent event, List<Permission> permissions){
-        this(event);
-        this.permissions.addAll(permissions);
-    }
-
-    public Integer getMinArguments() {
-        return minArguments;
-    }
-
-    public void setMinArguments(Integer minArguments) {
-        this.minArguments = minArguments;
-    }
-
-    public List<Permission> getPermissions() {
-        return permissions;
-    }
-
-    public void setPermissions(List<Permission> permissions) {
-        this.permissions = permissions;
-    }
-
-    public Map<Integer, String> getArguments() {
-        return arguments;
-    }
-
-    public void setArguments(Map<Integer, String> arguments) {
         this.arguments = arguments;
+    }
+
+    /**
+     * Gets the {@link MessageChannel} the {@link Message} was created in, if present.
+     * This may not be available if the {@code Message} was sent in a private channel.
+     *
+     * @return A {@link Mono} where, upon successful completion, emits the {@link MessageChannel} the message was created in,
+     * if present.
+     */
+    public Mono<MessageChannel> getMessageChannel(){
+        return message.getChannel();
+    }
+
+    /**
+     * Gets the {@link GuildChannel} the {@link Message} was created in, if present.
+     * This may not be available if the {@code Message} was sent in a private channel.
+     *
+     * @return A {@link Mono} where, upon successful completion, emits the {@link GuildChannel} the message was created in,
+     * if present.
+     */
+    public Mono<GuildChannel> getGuildChannel(){
+        return message.getChannel().ofType(GuildChannel.class);
+    }
+
+    /**
+     * Gets the {@link Guild} the {@link Message} was created in, if present.
+     * This may not be available if the {@code Message} was sent in a private channel.
+     *
+     * @return A {@link Mono} where, upon successful completion, emits the {@link Guild} the message was created in,
+     * if present.
+     */
+    public Mono<Guild> getGuild(){
+        return message.getGuild();
+    }
+
+    /**
+     * Gets the channelId of the {@link Channel} where the message was sent.
+     *
+     * @return The channelId of the {@link Channel}.
+     */
+    public String getChannelId(){
+        return message.getChannelId().asString();
+    }
+
+    /**
+     * Gets the nickname of the {@link Member} that sent the message, if present. Otherwise gets the display name
+     * of the {@link Member}.
+     *
+     * @return The nickname of the {@link Member}, if present. Otherwise, the display name.
+     */
+    public String getNickname(){
+        if (member == null) return "";
+        return member.getNickname().orElse(member.getDisplayName());
     }
 }
