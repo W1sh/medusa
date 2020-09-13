cc456c395386351d11b8a988073ce94b72f89003
W1sh <brunoferreira.ebm@gmail.com>
2020-09-13 19:09:34 +0100

    Improve handling of fragment messages

--- common/src/main/java/com/w1sh/medusa/services/MessageService.java
+++ common/src/main/java/com/w1sh/medusa/services/MessageService.java
@@ -2,10 +2,8 @@ package com.w1sh.medusa.services;
 
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
-import com.w1sh.medusa.data.responses.Embed;
 import com.w1sh.medusa.data.responses.MessageEnum;
 import com.w1sh.medusa.data.responses.Response;
-import com.w1sh.medusa.data.responses.TextMessage;
 import discord4j.core.object.entity.Message;
 import discord4j.core.object.entity.channel.MessageChannel;
 import discord4j.core.spec.EmbedCreateSpec;
@@ -13,35 +11,61 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.MessageSource;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.stereotype.Component;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.FluxProcessor;
+import reactor.core.publisher.FluxSink;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.UnicastProcessor;
 
 import java.time.Duration;
 import java.util.Locale;
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Consumer;
 
 @Slf4j
 @Component
 public class MessageService {
 
-    private final FluxProcessor<Response, Response> responseProcessor;
-    private final Flux<Response> responseFlux;
+    private final Map<String, SortedSet<Response>> responseMap = new ConcurrentHashMap<>();
+    private final FluxSink<Response> fluxSink;
     private final Cache<String, Message> messageCache;
     private final MessageSource messageSource;
 
     public MessageService(ResourceBundleMessageSource messageSource) {
+        final FluxProcessor<Response, Response> fluxProcessor = UnicastProcessor.create();
         this.messageSource = messageSource;
-        this.responseProcessor = UnicastProcessor.create();
-        this.responseFlux = responseProcessor.publish().autoConnect();
+        this.fluxSink = fluxProcessor.sink();
         this.messageCache = Caffeine.newBuilder()
-                .expireAfterAccess(Duration.ofHours(6))
-                .expireAfterWrite(Duration.ofDays(1))
+                .expireAfterAccess(Duration.ofHours(2))
+                .expireAfterWrite(Duration.ofHours(6))
                 .build();
+
+        fluxProcessor.map(x -> x)
+                .doOnEach(responseSignal -> log.debug("Received signal {} in message processor", responseSignal.getType().toString()))
+                .flatMap(response -> response.getMessageChannelMono()
+                        .flatMap(messageChannel -> messageChannel.createEmbed(response.getEmbedCreateSpec())))
+                .subscribe();
     }
 
-    public void queue(Response response) { Mono.just(response).subscribe(responseProcessor::onNext); }
+    public void queue(Response response) {
+        SortedSet<Response> responses;
+        if (!responseMap.containsKey(response.getChannelId())) {
+            responses = new TreeSet<>();
+        } else {
+            responses = responseMap.get(response.getChannelId());
+        }
+        responses.add(response);
+        responseMap.put(response.getChannelId(), responses);
+    }
+
+    public Mono<Message> sendOrQueue(Mono<MessageChannel> channelMono, Response response) {
+        if(response.isFragment()) {
+            queue(response);
+            return Mono.empty();
+        } else return send(channelMono, response.getEmbedCreateSpec());
+    }
 
     public Mono<Message> send(Mono<MessageChannel> channelMono, Consumer<EmbedCreateSpec> embedCreateSpec) {
         return channelMono.flatMap(channel -> channel.createEmbed(embedCreateSpec))
@@ -63,31 +87,14 @@ public class MessageService {
                 .doOnNext(message -> messageCache.put(message.getId().asString(), message));
     }
 
-    public void flush(){
-        responseFlux.filter(response -> !response.isFragment())
-                .flatMap(this::send)
-                .doOnNext(message -> messageCache.put(message.getId().asString(), message))
-                .subscribe()
-                .dispose();
-    }
-
-    public void flush(Long bufferSize){
-        responseFlux.take(bufferSize)
-                .sort()
-                .flatMap(this::send)
-                .doOnNext(message -> messageCache.put(message.getId().asString(), message))
-                .subscribe();
+    public void flush(String id){
+        SortedSet<Response> responses = responseMap.getOrDefault(id, new TreeSet<>());
+        responses.forEach(fluxSink::next);
+        responses.clear();
     }
 
     private String getMessage(String messageKey, String[] args) {
         return messageSource.getMessage(messageKey, args, Locale.ENGLISH);
     }
 
-    private Mono<Message> send(Response response){
-        if(response instanceof TextMessage) {
-            return response.getChannel().createMessage(((TextMessage) response).getContent());
-        }else if(response instanceof Embed){
-            return response.getChannel().createEmbed(((Embed) response).getEmbedCreateSpec());
-        }else return Mono.empty();
-    }
 }
--- mtg/src/main/java/com/w1sh/medusa/listeners/CardImageListener.java
+++ mtg/src/main/java/com/w1sh/medusa/listeners/CardImageListener.java
@@ -1,16 +1,20 @@
 package com.w1sh.medusa.listeners;
 
 import com.w1sh.medusa.data.events.InlineEvent;
-import com.w1sh.medusa.data.responses.Embed;
-import com.w1sh.medusa.services.MessageService;
+import com.w1sh.medusa.data.responses.Response;
 import com.w1sh.medusa.events.CardImageEvent;
 import com.w1sh.medusa.resources.Card;
 import com.w1sh.medusa.services.CardService;
+import com.w1sh.medusa.services.MessageService;
 import com.w1sh.medusa.utils.CardUtils;
+import discord4j.core.object.entity.Message;
+import discord4j.core.spec.EmbedCreateSpec;
 import discord4j.rest.util.Color;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
+
+import java.util.function.Consumer;
 
 @Component
 @RequiredArgsConstructor
@@ -18,29 +22,31 @@ public final class CardImageListener implements CustomEventListener<CardImageEve
 
     private final CardService cardService;
     private final MessageService messageService;
+    private final CardUtils cardUtils;
 
     @Override
     public Mono<Void> execute(CardImageEvent event) {
         return Mono.just(event)
                 .filter(InlineEvent::hasArgument)
                 .flatMap(ev -> cardService.getCardByName(ev.getInlineArgument()))
-                .flatMap(tuple -> createEmbed(tuple, event))
-                .doOnNext(messageService::queue)
-                .doAfterTerminate(messageService::flush)
+                .flatMap(tuple -> createCardImageEmbed(tuple, event))
                 .then();
     }
 
-    private Mono<Embed> createEmbed(Card card, CardImageEvent event){
-        return event.getChannel().map(channel -> {
-            if(card.isEmpty() || card.getUri() == null || card.getName() == null || card.getImage() == null || card.getImage().getNormal() == null){
-                return CardUtils.createErrorEmbed(channel, event);
-            }
-            return new Embed(channel, embedCreateSpec -> {
-                embedCreateSpec.setColor(Color.GREEN);
-                embedCreateSpec.setUrl(card.getUri());
-                embedCreateSpec.setTitle(card.getName());
-                embedCreateSpec.setImage(card.getImage().getNormal());
-            }, event.isFragment(), event.getInlineOrder());
-        });
+    private Mono<Message> createCardImageEmbed(Card card, CardImageEvent event){
+        if(card.isEmpty() || card.getUri() == null || card.getName() == null || card.getImage() == null || card.getImage().getNormal() == null){
+            return cardUtils.createErrorEmbed(event);
+        }
+
+        final Consumer<EmbedCreateSpec> specConsumer = embedCreateSpec -> {
+            embedCreateSpec.setColor(Color.GREEN);
+            embedCreateSpec.setUrl(card.getUri());
+            embedCreateSpec.setTitle(card.getName());
+            embedCreateSpec.setImage(card.getImage().getNormal());
+        };
+
+        final Response response = Response.with(specConsumer, event.getChannel(), event.getChannelId(),
+                event.isFragment(), event.getInlineOrder());
+        return messageService.sendOrQueue(event.getChannel(), response);
     }
 }
