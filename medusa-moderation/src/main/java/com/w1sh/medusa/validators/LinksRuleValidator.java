1588c72767ba5672ea86d7791eef2e1164c1596b
W1sh <brunoferreira.ebm@gmail.com>
2020-09-15 19:19:56 +0100

    Change links validation to specific Validator
    Related to #129
    
    Change links validation to specific Validator
    
    Related to #129

---
+++ medusa-moderation/src/main/java/com/w1sh/medusa/validators/LinksRuleValidator.java
@@ -0,0 +1,77 @@
+package com.w1sh.medusa.validators;
+
+import com.w1sh.medusa.data.Rule;
+import com.w1sh.medusa.data.Warning;
+import com.w1sh.medusa.data.responses.MessageEnum;
+import com.w1sh.medusa.services.ChannelService;
+import com.w1sh.medusa.services.MessageService;
+import com.w1sh.medusa.services.WarningService;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.event.domain.message.MessageUpdateEvent;
+import discord4j.core.object.entity.Member;
+import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.channel.GuildChannel;
+import discord4j.core.object.entity.channel.MessageChannel;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
+import static com.w1sh.medusa.utils.Reactive.ifElse;
+import static com.w1sh.medusa.utils.Reactive.isEmpty;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public final class LinksRuleValidator implements Validator<MessageCreateEvent> {
+
+    private final Pattern p = Pattern.compile("^((https?|ftp)://|(www|ftp)\\.)?[a-z0-9-]+(\\.[a-z0-9-]+)+([/?].*)?$");
+    private final ChannelService channelService;
+    private final WarningService warningService;
+    private final MessageService messageService;
+
+    @Override
+    public Mono<Boolean> validate(MessageCreateEvent event) {
+        return event.getMessage().getChannel()
+                .filterWhen(this::containsLinksRule)
+                .map(ignored -> event.getMessage().getContent())
+                .flatMap(this::containsLinks)
+                .transform(ifElse(b -> enforce(event), b-> Mono.empty()))
+                .transform(isEmpty());
+    }
+
+    public Mono<Boolean> validate(MessageUpdateEvent event) {
+        return event.getChannel()
+                .filterWhen(this::containsLinksRule)
+                .flatMap(ignored -> event.getMessage())
+                .map(Message::getContent)
+                .flatMap(this::containsLinks);
+    }
+
+    public Mono<Message> enforce(MessageCreateEvent event) {
+        final Mono<Message> warningMessage = messageService.send(event.getMessage().getChannel(), MessageEnum.NOLINKS,
+                event.getMember().map(Member::getDisplayName).orElse(""));
+
+        return warningService.addWarning(new Warning(event))
+                .flatMap(ignored -> event.getMessage().delete())
+                .onErrorResume(t -> Mono.fromRunnable(() -> log.error("Failed to delete message", t)))
+                .then(warningMessage);
+    }
+
+    private Mono<Boolean> containsLinks(String content) {
+        return Flux.fromIterable(Arrays.asList(content.split(" ")))
+                .filter(s -> p.matcher(s).find())
+                .hasElements();
+    }
+
+    private Mono<Boolean> containsLinksRule(MessageChannel messageChannel) {
+        return Mono.just(messageChannel).ofType(GuildChannel.class)
+                .flatMap(channelService::findByChannel)
+                .filter(channel -> channel.getRules().contains(Rule.NO_LINKS))
+                .hasElement();
+    }
+}
