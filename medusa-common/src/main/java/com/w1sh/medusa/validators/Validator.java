183acd6143cda41380d1e88cf4793a7fce72cb4e
W1sh <brunoferreira.ebm@gmail.com>
2020-09-17 12:01:25 +0100

    Add documentation to Validator

--- medusa-common/src/main/java/com/w1sh/medusa/validators/Validator.java
+++ medusa-common/src/main/java/com/w1sh/medusa/validators/Validator.java
@@ -2,7 +2,20 @@ package com.w1sh.medusa.validators;
 
 import reactor.core.publisher.Mono;
 
+/**
+ *
+ *
+ * @param <T> The type of the value evaluated.
+ */
 public interface Validator<T> {
 
-    Mono<Boolean> validate(T event);
+    /**
+     * Returns a {@link Mono} that represents the boolean evaluation of the item.
+     * @see ArgumentValidator
+     *
+     * @param item The item to validate.
+     * @return A {@link Mono} where, upon successful completion, emits the {@link Boolean}. If an error is received,
+     * it is emitted through the {@code Mono}.
+     */
+    Mono<Boolean> validate(T item);
 }
